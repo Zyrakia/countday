1092e0557eeacfd5934fb8084981e5572673d883
Zyrakia <ole.lammers@pm.me>
2025-05-16 20:45:37 -0400

    feat: remaining CRUD for batch

--- src/service/batch.ts
+++ src/service/batch.ts
@@ -1,13 +1,75 @@
 import { and, eq, SQL } from 'drizzle-orm';
 
 import { db } from '../db/db';
-import { Batch, batchTable } from '../db/schema';
+import { Batch, batchTable, insertBatchSchema, updateBatchSchema } from '../db/schema';
 import { createOrderByValue, OrderByDefinition } from '../util/order-by-build';
+import { z } from 'zod';
+import { ItemService } from './item';
 
 /**
  * Handles CRUD operations for batches.
  */
 export namespace BatchService {
+	/**
+	 * Creates a new batch.
+	 *
+	 * @param batch the properties of the batch
+	 * @return the created batch, or nothing if the insert didn't occur
+	 */
+	export async function insert(batch: z.infer<typeof insertBatchSchema>) {
+		const item = await ItemService.getOne(batch.itemId);
+		if (!item) return;
+
+		const [inserted] = await db
+			.insert(batchTable)
+			.values({
+				...batch,
+				locationId: batch.locationId ?? item.defaultLocationId,
+				supplierId: batch.supplierId ?? item.defaultSupplierId,
+			})
+			.returning();
+
+		if (inserted) return inserted;
+	}
+
+	/**
+	 * Updates an existing batch by ID.
+	 *
+	 * @param id the ID of the batch to update
+	 * @param partial the values to update on the batch
+	 * @return the updated batch, or nothing if the update didn't occur
+	 */
+	export async function update(id: string, partial: z.infer<typeof updateBatchSchema>) {
+		const [updated] = await db
+			.update(batchTable)
+			.set(partial)
+			.where(eq(batchTable.id, id))
+			.returning();
+
+		if (updated) return updated;
+	}
+
+	/**
+	 * Removes a batch by ID.
+	 *
+	 * @param id the ID of the batch to remove
+	 * @return the deleted batch, or nothing if the deletion didn't occur
+	 */
+	export async function remove(id: string) {
+		const [deleted] = await db.delete(batchTable).where(eq(batchTable.id, id)).returning();
+		if (deleted) return deleted;
+	}
+
+	/**
+	 * Returns a batch by ID.
+	 *
+	 * @param id the ID of the batch to retrieve
+	 * @return the batch, or nothing if the ID is invalid
+	 */
+	export async function getOne(id: string) {
+		return await db.query.batchTable.findFirst({ where: (batch, { eq }) => eq(batch.id, id) });
+	}
+
 	/**
 	 * Returns all batches for an item.
 	 *
