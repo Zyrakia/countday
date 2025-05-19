6ab752a767eaa089d744131f92234a19334c1530
Zyrakia <ole.lammers@pm.me>
2025-05-19 10:24:04 -0400

    refactor: removed initiating transactions directly from a service

--- src/util/create-service.ts
+++ src/util/create-service.ts
@@ -1,28 +1,29 @@
 import { stringifyError } from './stringify-error';
 
-type DBClient<C> = {
-	transaction<T>(fn: (tx: C) => Promise<T>): Promise<T>;
-};
-
 export type ServiceResult<T> = { success: true; data: T } | { success: false; error: string };
 
 type UnwrapPromise<T> = T extends Promise<infer U> ? U : T;
 
-type Actions<C extends DBClient<unknown>> = Record<string, (client: C, ...args: any[]) => any>;
-type MappedActions<C extends DBClient<unknown>, A extends Actions<C>> = {
+type Actions<C> = Record<string, (client: C, ...args: any[]) => any>;
+
+type MappedActions<C, A extends Actions<C>> = {
 	[K in keyof A]: A[K] extends (c: C, ...args: infer P) => infer R
 		? (...args: P) => Promise<ServiceResult<UnwrapPromise<R>>>
 		: never;
 };
 
-type Helpers<C extends DBClient<unknown>, A extends Actions<C>> = {
-	tx: <T>(fn: (txService: Service<C, A>) => Promise<T>) => Promise<ServiceResult<T>>;
-	withClient: <C2 extends C>(client: C2) => Service<C2, A>;
-	client: C;
+type Helpers<C, A extends Actions<C>> = {
+	/**
+	 * Creates a new service that mirrors the actions of the current service,
+	 * but performs all actions on a new client.
+	 *
+	 * @param client the new client of the service
+	 * @return the mirroring service with a new client
+	 */
+	$with: <C2 extends C>(client: C2) => Service<C2, A>;
 };
 
-export type Service<C extends DBClient<unknown>, A extends Actions<C>> = MappedActions<C, A> &
-	Helpers<C, A>;
+export type Service<C extends unknown, A extends Actions<C>> = MappedActions<C, A> & Helpers<C, A>;
 
 /**
  * Creates a new service with an initial client.
@@ -31,13 +32,12 @@ export type Service<C extends DBClient<unknown>, A extends Actions<C>> = MappedA
  * @param actions the actions that the service exposes
  * @return the created service with mapped actions
  */
-export function createService<C extends DBClient<unknown>, A extends Actions<C>>(
+export function createService<C extends unknown, A extends Actions<C>>(
 	client: C,
 	actions: A,
 ): Service<C, A> {
-	const wrap =
-		<Args extends any[], R>(fn: (client: C, ...args: Args) => Promise<R> | R) =>
-		async (...args: Args): Promise<ServiceResult<R>> => {
+	const wrap = <Args extends any[], R>(fn: (client: C, ...args: Args) => Promise<R> | R) => {
+		return async (...args: Args): Promise<ServiceResult<R>> => {
 			try {
 				const data = await fn(client, ...args);
 				return { success: true, data };
@@ -45,31 +45,16 @@ export function createService<C extends DBClient<unknown>, A extends Actions<C>>
 				return { success: false, error: stringifyError(err) };
 			}
 		};
+	};
 
 	const serviceMethods = Object.fromEntries(
-		Object.entries(actions).map(([key, fn]) => [key, wrap(fn as any)]),
+		Object.entries(actions).map(([key, action]) => [key, wrap(action)]),
 	) as MappedActions<C, A>;
-
-	async function tx<T>(fn: (txService: Service<C, A>) => Promise<T>) {
-		return client.transaction(async (txClient): Promise<ServiceResult<T>> => {
-			const txService = createService(txClient as C, actions);
-			try {
-				const data = await fn(txService);
-				return { success: true, data };
-			} catch (err) {
-				return { success: false, error: stringifyError(err) };
-			}
-		});
-	}
-
-	function withClient<C2 extends C>(client: C2) {
-		return createService(client, actions);
-	}
 
 	return {
 		...serviceMethods,
-		tx,
-		withClient,
-		client,
+		$with: <C2 extends C>(newClient: C2) => {
+			return createService(newClient, actions);
+		},
 	};
 }
